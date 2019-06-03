f275d6c825d909e68e805a85f27f2cb88a525c41
Linux Lite <shashank.araokar@gmail.com>
2019-06-03 01:17:51 -0400

    Added tests to verify slot resolver functionality

--- src/test/java/wavicle/simpledsl/SimpleDSLTestSuite.java
+++ src/test/java/wavicle/simpledsl/SimpleDSLTestSuite.java
@@ -60,4 +60,48 @@ public class SimpleDSLTestSuite {
 		assertEquals("Java", result.getSlotValue("propertyValue").getLiteral());
 	}
 
+	/**
+	 * Verifies that the SlotResolver interface can be used in 'sanitizing' or
+	 * 'resolving' literal slot values. In this example, the literal slot value is
+	 * just made all upper-case, but more complex resolution can be programmed as
+	 * needed.
+	 */
+	@Test
+	public void simple_slot_resolver() {
+		/** Create a simple intent **/
+		Intent intent = new Intent();
+		intent.addSampleUtterance("I have lived in (?<cityName>\\w+) since (?<year>\\w+)");
+		/** Add a slot resolver that makes the city name uppercase **/
+		intent.addSlotResolver("cityName", new SlotResolver() {
+
+			@Override
+			public String resolve(String literal) {
+				return literal.toUpperCase();
+			}
+		});
+
+		/** Create a grammar just with one intent **/
+		Grammar grammar = new Grammar();
+		grammar.addIntent(intent);
+
+		/** We pass an input where the city name is all lowercase **/
+		ComprehensionResult result = grammar.comprehend("I have lived in boston since 2014");
+
+		/**
+		 * Verify that the literal match is still lower case, but the resolved value is
+		 * all uppercase
+		 **/
+		SlotValue cityNameSlotValue = result.getSlotValue("cityName");
+		assertEquals("boston", cityNameSlotValue.getLiteral());
+		assertEquals("BOSTON", cityNameSlotValue.getResolved());
+
+		/**
+		 * Also verify that if no resolver is specified, the literal and resolved values
+		 * are identical
+		 **/
+		SlotValue yearSlotValue = result.getSlotValue("year");
+		assertEquals("2014", yearSlotValue.getLiteral());
+		assertEquals("2014", yearSlotValue.getResolved());
+	}
+
 }
